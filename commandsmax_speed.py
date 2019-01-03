324dbe38b54491b8c183269830651c2a5fe3fccb
Nakul Shah <nshah@bluemoon.cgs.wustl.edu>
2019-01-03 11:18:50 -0600

    Fixed samtools error messages that could make log and output files way too large

--- commandsmax_speed.py
+++ commandsmax_speed.py
@@ -29,7 +29,7 @@ with open(sys.argv[1],"r") as filteredfile:
 			elementvecint = [int(x) for x in elementsvec.split(',')]
 			startloc = min(elementvecint) - distbuffer
 			endloc = max(elementvecint) + distbuffer
-			commandroot = 'samtools view -q 255 -h -f 2 -F 4 ' + bamfileloc + filename + '.bam ' + chromosome + ':' + str(startloc) + '-' + str(endloc) + " > " + uniqid + '--' + filename + ".sam" + ' ; samtools view -q 255 -h -f 2 -F 4 ' + bamfileloc + filename + '.bam ' + chromosome + ':' + str(startlocTE) + '-' + str(endlocTE) + ' | cut -f1  | sort | uniq > ' + uniqid + '--' + filename + 'IDs.txt ; cat <(samtools view -H ' + uniqid + '--' + filename + '.sam) <(grep -w -F -f ' + uniqid + '--' + filename + 'IDs.txt ' + uniqid + '--' + filename + '.sam)'
+			commandroot = 'samtools view -q 255 -h -f 2 -F 4 ' + bamfileloc + filename + '.bam ' + chromosome + ':' + str(startloc) + '-' + str(endloc) + " > " + uniqid + '--' + filename + ".sam" + ' ; samtools view -q 255 -f 2 -F 4 ' + bamfileloc + filename + '.bam ' + chromosome + ':' + str(startlocTE) + '-' + str(endlocTE) + ' | cut -f1  | sort | uniq > ' + uniqid + '--' + filename + 'IDs.txt ; cat <(samtools view -H ' + uniqid + '--' + filename + '.sam) <(grep -w -F -f ' + uniqid + '--' + filename + 'IDs.txt ' + uniqid + '--' + filename + '.sam)'
 		else:
 			strand = temp[37]
 			elementvecint = [int(x) for x in elementsvec.split(',')]
@@ -39,7 +39,7 @@ with open(sys.argv[1],"r") as filteredfile:
 			else:
 				startloc = min(elementvecint) - distbuffer
 				endloc = int(temp[33]) + distbuffer
-			commandroot = 'samtools view -q 255 -h -f 2 -F 4 ' + bamfileloc + filename + '.bam ' + chromosome + ':' + str(startloc) + '-' + str(endloc) + " > " + uniqid + '--' + filename + ".sam" + ' ; samtools view -q 255 -h -f 2 -F 4 ' + bamfileloc + filename + '.bam ' + chromosome + ':' + str(startlocTE) + '-' + str(endlocTE) + ' | cut -f1  | sort | uniq > ' + uniqid + '--' + filename + 'IDs.txt ; cat <(samtools view -H ' + uniqid + '--' + filename + '.sam) <(grep -w -F -f ' + uniqid + '--' + filename + 'IDs.txt ' + uniqid + '--' + filename + '.sam)'
+			commandroot = 'samtools view -q 255 -h -f 2 -F 4 ' + bamfileloc + filename + '.bam ' + chromosome + ':' + str(startloc) + '-' + str(endloc) + " > " + uniqid + '--' + filename + ".sam" + ' ; samtools view -q 255 -f 2 -F 4 ' + bamfileloc + filename + '.bam ' + chromosome + ':' + str(startlocTE) + '-' + str(endlocTE) + ' | cut -f1  | sort | uniq > ' + uniqid + '--' + filename + 'IDs.txt ; cat <(samtools view -H ' + uniqid + '--' + filename + '.sam) <(grep -w -F -f ' + uniqid + '--' + filename + 'IDs.txt ' + uniqid + '--' + filename + '.sam)'
 
 
 		commandfinish = ' | samtools sort -n -m 1G - | bedtools bamtobed -bedpe -i stdin 2>/dev/null | ' + sys.path[0] + '/rmsk_annotate_bedpe_speed.py ' + chromosome + ',' + startlocTE + ',' + endlocTE + ' ' + elementsvec + ' ' + strand + ' ' + number1 + ' ' + number2 + ' > ./filterreadstats/' +  uniqid + '--' + filename + ".stats ; " + "rm " +  uniqid + '--' + filename + ".sam ; rm " +  uniqid + '--' + filename + 'IDs.txt'
